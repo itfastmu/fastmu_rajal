741ee885514f73220afd33d7a5ed88a6ceff0fd2
itfastmu <it.fastmupati@gmail.com>
2023-03-08 10:49:11 +0700

    first init rajal app

---
+++ android/app/src/main/kotlin/com/example/fastmu_healthcare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fastmu_healthcare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello !"),
+        ),
+      )
+    );
+}
+}
+
+// Fastmu Rawat inap
